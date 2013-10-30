b81812f954a0b2c85baf949074165ce6417c34d5
zoomby <zoomby@list.ru>
2013-10-30 13:38:41 +0400

    Initial version

---
+++ IndicativeRateUSD/MainForm.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace IndicativeRateUSD
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnDeals = new System.Windows.Forms.Button();
+            this.btnUSD = new System.Windows.Forms.Button();
+            this.btnResult = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDeals
+            // 
+            this.btnDeals.Location = new System.Drawing.Point(23, 12);
+            this.btnDeals.Name = "btnDeals";
+            this.btnDeals.Size = new System.Drawing.Size(116, 23);
+            this.btnDeals.TabIndex = 0;
+            this.btnDeals.Text = "Сделки";
+            this.btnDeals.UseVisualStyleBackColor = true;
+            this.btnDeals.Click += new System.EventHandler(this.btnDeals_Click);
+            // 
+            // btnUSD
+            // 
+            this.btnUSD.Enabled = false;
+            this.btnUSD.Location = new System.Drawing.Point(23, 41);
+            this.btnUSD.Name = "btnUSD";
+            this.btnUSD.Size = new System.Drawing.Size(116, 23);
+            this.btnUSD.TabIndex = 0;
+            this.btnUSD.Text = "USD";
+            this.btnUSD.UseVisualStyleBackColor = true;
+            this.btnUSD.Click += new System.EventHandler(this.btnUSD_Click);
+            // 
+            // btnResult
+            // 
+            this.btnResult.Enabled = false;
+            this.btnResult.Location = new System.Drawing.Point(23, 70);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(116, 23);
+            this.btnResult.TabIndex = 0;
+            this.btnResult.Text = "Подобрать курсы";
+            this.btnResult.UseVisualStyleBackColor = true;
+            this.btnResult.Click += new System.EventHandler(this.btnResult_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.btnResult);
+            this.Controls.Add(this.btnUSD);
+            this.Controls.Add(this.btnDeals);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Индикативные курсы USD";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDeals;
+        private System.Windows.Forms.Button btnUSD;
+        private System.Windows.Forms.Button btnResult;
+    }
+}
+
---
+++ IndicativeRateUSD/MainForm.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace IndicativeRateUSD
+{
+    public partial class MainForm : Form
+    {
+        string[] deals;
+        string[] usd;
+        string[] output;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }        
+
+        private void btnDeals_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog open_file = new OpenFileDialog())
+            {
+                open_file.Filter = "Text files (*.txt) | *.txt";
+
+                if (open_file.ShowDialog() == DialogResult.OK)
+                {
+                    deals = File.ReadAllLines(open_file.FileName);
+                    btnUSD.Enabled = true;
+                }
+                else { return; }
+
+                output = new string[deals.Length];                
+            }
+        }
+
+        private void btnUSD_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog open_file = new OpenFileDialog())
+            {
+                open_file.Filter = "Text files (*.txt) | *.txt";
+
+                if (open_file.ShowDialog() == DialogResult.OK)
+                {
+                    string[] temp_mass = File.ReadAllLines(open_file.FileName);
+
+                    usd = new string[temp_mass.Length / 2];
+
+                    int j = -1;
+                    string[] row_1;
+                    string[] row_2;
+                    string temp_str;
+
+                    for (int i = temp_mass.Length - 1; i > 0; i = i - 2)
+                    {
+                        row_1 = temp_mass[i].Split(new char[] { '\t', ' ' });
+                        row_2 = temp_mass[i - 1].Split(new char[] { '\t', ' ' });
+
+                        if (row_1[1] == row_2[1] && row_1[3] != row_2[3])
+                        {
+                            temp_str = string.Format("{0}\t{1}\t{2}", row_1[1], row_1[3], row_2[3]);
+                            j++;
+                        }
+                        else
+                        {
+                            MessageBox.Show(string.Format("Ошибка в индикативных курсах в строках {0} и {1}!!!", i - 1, i));
+                            break;
+                        }
+
+                        usd[j] = temp_str;
+                    }
+
+                    btnResult.Enabled = true;
+
+                }
+                else { return; }
+            }
+        }
+
+        private void btnResult_Click(object sender, EventArgs e)
+        {
+            const int CONST_DATE = 0;
+            const int CONST_TIME = 1;
+            const int CONST_QUOTE_1 = 1;
+            const int CONST_QUOTE_2 = 2;
+
+            DateTime date_deal;
+            DateTime date_usd;
+            DateTime time_deal;
+
+            string[] temp_deal;
+            string[] temp_usd;
+            string temp_str = "";
+
+            bool flag;
+
+            for (int i = 0; i < deals.Length; i++)
+            {
+                flag = true;
+
+                temp_deal = deals[i].Split(new char[] { '\t' });
+                date_deal = DateTime.Parse(temp_deal[CONST_DATE]);
+
+                for (int j = 0; j < usd.Length; j++)
+                {
+                    if (flag)
+                    {
+                        temp_usd = usd[j].Split(new char[] { '\t' });
+                        date_usd = DateTime.Parse(temp_usd[CONST_DATE]);
+
+                        if (date_deal == date_usd)
+                        {
+                            time_deal = DateTime.Parse(temp_deal[CONST_TIME], null, System.Globalization.DateTimeStyles.NoCurrentDateDefault);
+
+                            if (time_deal < new DateTime(1, 1, 1, 14, 0, 0))
+                            {
+                                if (j == 0)
+                                {
+                                    MessageBox.Show("Добавте более ранние данные об индикативных курсах.");
+                                    break;
+                                }
+                                else
+                                {
+                                    temp_usd = usd[j - 1].Split(new char[] { '\t' });
+                                    temp_str = string.Format("{0}\t{1}", temp_deal[CONST_DATE], temp_usd[CONST_QUOTE_2]);
+                                    flag = false;//прервать внутренний цикл
+
+                                }
+                            }
+                            else if (time_deal > new DateTime(1, 1, 1, 18, 45, 0))
+                            {
+                                temp_str = string.Format("{0}\t{1}", temp_deal[CONST_DATE], temp_usd[CONST_QUOTE_2]);
+                                flag = false;//прервать внутренний цикл
+                            }
+
+                            else
+                            {
+                                temp_str = string.Format("{0}\t{1}", temp_deal[CONST_DATE], temp_usd[CONST_QUOTE_1]);
+                                flag = false;//прервать внутренний цикл
+                            }
+
+                        }
+                        
+                    }
+                    
+                }
+
+                if (flag)//если во внутреннем цикле флаг так и остался true значит нужные данные не найдены
+                {
+                    MessageBox.Show("В файле нужные индикативные курсы не найдены. Добавьте данные.");
+                    return;
+                }
+
+                output[i] = temp_str;
+            }
+
+            using (SaveFileDialog save_file = new SaveFileDialog())
+            {
+                save_file.Filter = "Text files (*.txt) | *.txt";
+
+                if (save_file.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllLines(save_file.FileName, output);
+                    Close();
+                }
+                else { Close(); }
+            }
+        }
+    }
+}
